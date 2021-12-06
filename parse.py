bdd00ac77319948770ac20dcb32691b2e6ee04a2
Sabina Zova <95089786+sabina-zova@users.noreply.github.com>
2021-12-06 19:36:23 +0100

    extract function tests

--- parse.py
+++ parse.py
@@ -1,8 +1,20 @@
-import json
+# name, cpu a memory usage, created_at, status a všechny přiřazené IP adresy
 
+import json
+from extract import json_extract
+
+# Create a dictionary from sample data
 with open("sample-data.json", "r") as data:
     data_list = json.load(data)
 
-for d in data_list:
-    print(d)
+# Using extract function to extract values with the key 'name'
+names = json_extract(data_list, 'name')
+for name in names:
+    print(name)
+
+# Extract function does not work in case the given key contains more data structures
+config = json_extract(data_list, 'config')
+print(config)
+
+# TODO find a way to access data structures on upper levels
 
